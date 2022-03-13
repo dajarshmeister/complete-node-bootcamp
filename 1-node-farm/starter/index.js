82e776c120562d51539c2fbd50a7c9ada1808d06
Joshua Palmer <joshua_palmer@edwards.com>
2022-03-13 16:41:01 -0700

    starter

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,19 +1,51 @@
+/* ==== SERVER ==== */
+
+const http = require('http');
+const url = require('url');
 const fs = require('fs');
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-	if (err) return console.log('ERROR! 🤯');
-	fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-		console.log(data2);
-		fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-			console.log(data3);
-			fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-				console.log('File has been written!');
-			});
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+	const pathName = req.url;
+	if (pathName === '/' || pathName === '/overview') {
+		res.end('This is the OVERVIEW');
+	} else if (pathName === '/product') {
+		res.end('This is the PRODUCT');
+	} else if (pathName === '/api') {
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.end(data);
+	} else {
+		res.writeHead(404, {
+			'Content-Type': 'text/html',
+			'my-own-header': 'hello world',
 		});
-	});
+		res.end('<h1>This page cannot be found</h1>');
+	}
 });
 
-console.log('Will read file!');
+server.listen(8000, '127.0.0.1', () => {
+	console.log('Listening to requests on port 8000');
+});
+
+/* ==== FILES ==== */
+
+//const fs = require('fs');
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+// 	if (err) return console.log('ERROR! 🤯');
+// 	fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+// 		console.log(data2);
+// 		fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+// 			console.log(data3);
+// 			fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+// 				console.log('File has been written!');
+// 			});
+// 		});
+// 	});
+// });
+
+// console.log('Will read file!');
 
 //Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
