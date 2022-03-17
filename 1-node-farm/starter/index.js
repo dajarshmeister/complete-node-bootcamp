f5870f60c2f3d7076479a44343e20cf3523e738a
Joshua Palmer <joshua_palmer@edwards.com>
2022-03-16 21:42:45 -0700

    josh way

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,103 +1,39 @@
-/* ==== SERVER ==== */
-
 const http = require('http');
-const { URL } = require('url');
 const fs = require('fs');
 
-const replaceTemplate = (temp, product) => {
-	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-	output = output.replace(/{%ID%}/g, product.id);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	if (!product.organic) {
-		output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-	}
-	return output;
-};
+const products = JSON.parse(
+	fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+);
 
-const tempOverview = fs.readFileSync(
+const templateOverview = fs.readFileSync(
 	`${__dirname}/templates/template-overview.html`,
 	'utf-8'
 );
-const tempCard = fs.readFileSync(
+
+const templateCard = fs.readFileSync(
 	`${__dirname}/templates/template-card.html`,
 	'utf-8'
 );
-const tempProduct = fs.readFileSync(
-	`${__dirname}/templates/template-product.html`,
-	'utf-8'
-);
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-
-const dataObj = JSON.parse(data);
+const generateTemplate = (product, template) => {
+	let result = template.replace(/{%IMAGE%}/g, product.image);
+	result = result.replace(/{%PRODUCTNAME%}/g, product.productName);
+	result = result.replace(/{%QUANTITY%}/g, product.quantity);
+	result = result.replace(/{%PRICE%}/g, product.price);
+	result = result.replace(/{%ID%}/g, product.id);
+	return result;
+};
 
 const server = http.createServer((req, res) => {
-	const { pathname, searchParams } = new URL(req.url, 'http://127.0.0.1/');
-	const id = searchParams.get('id');
-
-	// Overview page
-	if (pathname === '/' || pathname === '/overview') {
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-		const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+	const { url } = req;
+	if (url === '/') {
+		res.writeHead(200);
+		const result = products
+			.map(product => generateTemplate(product, templateCard))
+			.join('');
+		const output = templateOverview.replace('{%PRODUCT_CARDS%}', result);
 		res.end(output);
-
-		// Product page
-	} else if (pathname === '/product') {
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		const product = dataObj[id];
-		const output = replaceTemplate(tempProduct, product);
-		res.end(output);
-
-		// API page
-	} else if (pathname === '/api') {
-		res.writeHead(200, { 'Content-Type': 'application/json' });
-		res.end(data);
-
-		// Not Found
-	} else {
-		res.writeHead(404, {
-			'Content-Type': 'text/html',
-			'my-own-header': 'hello world',
-		});
-		res.end('<h1>This page cannot be found</h1>');
 	}
 });
 
-server.listen(8000, '127.0.0.1', () => {
-	console.log('Listening to requests on port 8000');
-});
-
-/* ==== FILES ==== */
-
-//const fs = require('fs');
-// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-// 	if (err) return console.log('ERROR! 🤯');
-// 	fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-// 		console.log(data2);
-// 		fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-// 			console.log(data3);
-// 			fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-// 				console.log('File has been written!');
-// 			});
-// 		});
-// 	});
-// });
-
-// console.log('Will read file!');
-
-//Blocking, synchronous way
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(textIn);
-
-// const textOut = `This is what we know about avocado: ${textIn}. Created on ${Date.now()}`;
-
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('file written');
-
-//Non Blocking, asychronous way
+server.listen(8000);
