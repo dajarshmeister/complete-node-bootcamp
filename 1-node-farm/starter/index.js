dd48fca1eaed55057fb2657cd18ab68a3795ebdf
Joshua Palmer <joshua_palmer@edwards.com>
2022-03-17 21:45:17 -0700

    x

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,39 +1,58 @@
 const http = require('http');
+const { URL } = require('url');
 const fs = require('fs');
+const replaceTemplate = require('./modules/replaceTemplate');
 
-const products = JSON.parse(
-	fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-);
-
-const templateOverview = fs.readFileSync(
+const tempOverview = fs.readFileSync(
 	`${__dirname}/templates/template-overview.html`,
 	'utf-8'
 );
-
-const templateCard = fs.readFileSync(
+const tempCard = fs.readFileSync(
 	`${__dirname}/templates/template-card.html`,
 	'utf-8'
 );
+const tempProduct = fs.readFileSync(
+	`${__dirname}/templates/template-product.html`,
+	'utf-8'
+);
 
-const generateTemplate = (product, template) => {
-	let result = template.replace(/{%IMAGE%}/g, product.image);
-	result = result.replace(/{%PRODUCTNAME%}/g, product.productName);
-	result = result.replace(/{%QUANTITY%}/g, product.quantity);
-	result = result.replace(/{%PRICE%}/g, product.price);
-	result = result.replace(/{%ID%}/g, product.id);
-	return result;
-};
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-	const { url } = req;
-	if (url === '/') {
-		res.writeHead(200);
-		const result = products
-			.map(product => generateTemplate(product, templateCard))
-			.join('');
-		const output = templateOverview.replace('{%PRODUCT_CARDS%}', result);
+	const { pathname, searchParams } = new URL(req.url, 'http://127.0.0.1/');
+	const id = searchParams.get('id');
+
+	// Overview page
+	if (pathname === '/' || pathname === '/overview') {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+		const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 		res.end(output);
+
+		// Product page
+	} else if (pathname === '/product') {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		const product = dataObj[id];
+		const output = replaceTemplate(tempProduct, product);
+		res.end(output);
+
+		// API page
+	} else if (pathname === '/api') {
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.end(data);
+
+		// Not Found
+	} else {
+		res.writeHead(404, {
+			'Content-Type': 'text/html',
+			'my-own-header': 'hello world',
+		});
+		res.end('<h1>This page cannot be found</h1>');
 	}
 });
 
-server.listen(8000);
+server.listen(8000, '127.0.0.1', () => {
+	console.log('Listening to requests on port 8000');
+});
